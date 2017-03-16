55b3b3a51418545e4076a9979336cf4c1a0aa4bc
Myztiq <kahn@runnable.com>
2017-03-16 15:35:48 -0700

    Updated to no longer use base url

--- client/index.js
+++ client/index.js
@@ -1,10 +1,9 @@
-const request = require('request-promise').defaults({
-  baseUrl: process.env.CLIO_HTTP_URL
-})
+const request = require('request-promise')
+const baseUrl = process.env.CLIO_HTTP_URL
 
 module.exports = {
   fetchInstanceHistory: (instanceId) => {
-    return request.get(`instances/${instanceId}/history`, {
+    return request.get(`${baseUrl}instances/${instanceId}/history`, {
       json: true
     })
   }
