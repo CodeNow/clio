abc5b7a7574b3517f3ebe05c33e06724454d18d3
Myztiq <kahn@runnable.com>
2017-03-16 17:25:49 -0700

    Updated docs

--- client/index.js
+++ client/index.js
@@ -9,6 +9,11 @@ module.exports = {
   fetchInstanceHistory: (instanceId) => {
     return request.get(`${baseUrl}instances/${instanceId}/history`, defaultOptions)
   },
+  /**
+   * Given a container ID find an instance that has ran this container.
+   * In the case of builds this has the possibility of being multiple instances since we
+   * do build deduping. So.... we return the first instance for the container ID for the moment.
+   */
   fetchContainerInstance: (containerId) => {
     return request.get(`${baseUrl}containers/${containerId}/instance`, defaultOptions)
   }
